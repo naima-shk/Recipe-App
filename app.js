3e98417db828b5a13330a2e1f9310069020854a4
naima-shk <naimashaikh550@gmail.com>
2021-07-29 17:01:16 +0500

    'implement-recipe-app'

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let searchForm = document.querySelector('form');
 let searchResultDiv = document.querySelector('.search-result');
 let container = document.querySelector('.container');
-let searchQuery = '';
+let searchQuery = 'pasta';
 let API_KEY ='c9fbd55104db4d3a901694eda6081a80';
 
 
@@ -9,13 +9,23 @@ searchForm.addEventListener('submit', (e)=>{
     e.preventDefault();
     searchQuery=e.target.querySelector('input').value;
     console.log(searchQuery);
-    fetchAPI();
+    
 })
 
-async function fetchAPI(){
-    const baseURL = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=API_KEY&query=pasta'
-    const response = await fetch(baseURL);
-    const data = await response.json();
-    console.log(data);
-
-}
+fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=API_KEY&query=pasta').then(function (response){
+     //return response.json();
+    console.log(response, 'response')
+   /* document.getElementsByClassName('search-result')[0].innerHTML;
+      document.getElementsByClassName('search-result')[1].innerHTML;
+      document.getElementsByClassName('search-result')[3].innerHTML;
+      document.getElementsByClassName('search-result')[4].innerHTML;
+      document.getElementsByClassName('search-result')[5].innerHTML;*/
+  }).then (function (form){
+      
+      console.log(form);
+     
+  }).catch(function (error){
+      console.log('something went wrong');
+      console.error(error);
+  });
+  
